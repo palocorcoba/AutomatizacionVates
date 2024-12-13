895c7f0e455a051382136af9e045a45e13af338e
Ramiro Hosman <ramirohosman@gmail.com>
2024-12-13 09:02:21 -0300

    Agregando casos de Wiki de Ramiro

--- src/main/java/com/selenium/test/wikiTest2.java
+++ src/main/java/com/selenium/test/wikiTest2.java
@@ -5,9 +5,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.testng.Assert;
+import org.testng.ITestContext;
 import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import com.selenium.driver.DriverFactory;
@@ -18,17 +20,22 @@ public class wikiTest2 {
 	WebDriver driver;
 
 	@BeforeMethod
-	public void Precondicion() {
-		String url = "http://wikipedia.org";
-		String navegador = "CHROME";
-		//String navegador = "FIREFOX";
-//		String navegador = "EDGE";
-		driver = DriverFactory.LevantarBrowser(driver, url, navegador);
+	public void Precondicion(ITestContext context)  {
+		String browserName = context.getCurrentXmlTest().getParameter("NombreNavegador");
+		String URL = context.getCurrentXmlTest().getParameter("Url");
+		driver = DriverFactory.LevantarBrowser(driver, URL, browserName);
 	}
 
 	@AfterMethod
 	public void Postcondicion() {
 		DriverFactory.FinalizarBrowser(driver);
+	}
+	
+	@DataProvider(name = "datos")
+	public Object[][] createData() {
+	    return new Object[][] {
+	        {"Messi"}
+	    };
 	}
 
 	@Test(description = "Validar que las busquedas en Wikipedia funcionan")
@@ -41,6 +48,50 @@ public class wikiTest2 {
 		Assert.assertTrue((homepage.SeVisualizaCaja()), "No se visualizo la caja");
 
 		String dato = "Messi";
+
+		Reporter.log("Ingresar el texto " + dato, true);
+		homepage.IngresarDatoCajaBusqueda(dato);
+
+		WikiResultPage resultadoPage = PageFactory.initElements(driver, WikiResultPage.class);
+
+		Assert.assertTrue((resultadoPage.getTitulo(dato).contains(dato)), "no se encontro " + dato);
+	}
+	/**Ramiro 
+	 Fecha: 13/12/24
+	 Hora: 8:48
+	 **/
+	@Test(description = "Validar que las busquedas en Wikipedia funcionan")
+	public void ValidarBusquedaWikipedia() throws Exception {
+
+		Reporter.log("Página de Wikipedia cargada", true);
+		WebElement searchInput = driver.findElement(By.id("searchInput"));
+		Assert.assertTrue(searchInput.isDisplayed());
+		Reporter.log("Campo de búsqueda encontrado", true);
+		searchInput.sendKeys("Selenium");
+		Reporter.log("Texto 'Selenium' ingresado en el campo de búsqueda", true);
+
+		searchInput.submit();
+		Reporter.log("Formulario de búsqueda enviado", true);
+		
+		Thread.sleep(5000);
+		WebElement tituloResultado = driver.findElement(By.id("firstHeading"));
+		Reporter.log("Texto encontrado en la página: " + tituloResultado.getText(), true);
+
+		System.out.println("Texto encontrado " + tituloResultado.getText());
+		Assert.assertTrue(tituloResultado.isDisplayed());
+		Reporter.log("Título del resultado de búsqueda es visible", true);
+		Reporter.log("Prueba en Chrome finalizada", true);
+		driver.close();
+	}
+
+	@Test(dataProvider = "datos", description = "Validar que las busquedas en Wikipedia funcionan")
+	public void ValidarWikipediaRamiro(String dato) throws Exception {
+		Reporter.log("Caso de prueba Fanny Montoya y Mariana", true);
+		Reporter.log("Localizar la caja de busqueda del home de wiki", true);
+		WikiHomePage homepage = PageFactory.initElements(driver, WikiHomePage.class);
+
+		Reporter.log("Verificar que la caja de busqueda se este mostrando", true);
+		Assert.assertTrue((homepage.SeVisualizaCaja()), "No se visualizo la caja");
 
 		Reporter.log("Ingresar el texto " + dato, true);
 		homepage.IngresarDatoCajaBusqueda(dato);
